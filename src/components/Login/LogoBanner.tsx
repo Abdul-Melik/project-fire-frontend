160631e8915212aec4ab38d7313cae6c60090da7
Haris Velic <haris.velic@ses.edu.ba>
2023-04-18 12:16:15 +0200

    Completed Login Form connected with Backend.

--- src/components/Login/LogoBanner.tsx
+++ src/components/Login/LogoBanner.tsx
@@ -5,8 +5,8 @@ import logotype from '../../../public/svg/logotype.svg';
 
 function LeftScreen() {
   return (
-    <div className='screen'>
-  <div className='logo' style={{ backgroundImage: `url(${logotype})`, 
+    <div className="w-full flex items-center justify-center h-full bg-[url('../../public/svg/gradientbg.svg')] bg-no-repeat bg-cover">
+  <div className="w-full h-1/2 bg-cover bg-center" style={{ backgroundImage: `url(${logotype})`, 
   backgroundSize: "50% auto", 
   backgroundRepeat: "no-repeat", }} />
 </div>
---
+++ src/shared/hooks/auth-hook.ts
@@ -0,0 +1,61 @@
+import { useState, useCallback, useEffect } from "react";
+
+let logoutTimer: ReturnType<typeof setTimeout>;
+
+const useAuth = () => {
+  const [token, setToken] = useState<string | null>(null);  
+  const [userId, setUserId] = useState<string | null>(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState<Date | null>(null);
+  const [expiresIn, setExpiresIn] = useState<number>(0)
+  
+  const login = useCallback((token: string, expiresIn: number, userId: string,  expirationDate?: Date  ) => {
+    setToken(token);
+    setUserId(userId);
+    setExpiresIn(expiresIn);
+
+    const tokenLoginExpirationDate =
+      expirationDate || new Date(new Date().getTime() + expiresIn); 
+    setTokenExpirationDate(tokenLoginExpirationDate);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        token,
+        userId,
+        expiration: tokenLoginExpirationDate.toLocaleString(),
+      })
+    );
+  }, []);
+
+  const logout = useCallback(() => {
+    setToken("");
+    setUserId(null); 
+    setTokenExpirationDate(null);
+    localStorage.removeItem("userData");
+    clearTimeout(logoutTimer);
+  }, [logoutTimer]);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+    return () => clearTimeout(logoutTimer);
+  }, [token, tokenExpirationDate, logout]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData") || "null");
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(storedData.token, expiresIn, storedData.userId, new Date(storedData.expiration));
+    }
+  }, [login]);
+ 
+  return { token, userId, login, logout };
+};
+
+export default useAuth;
