5d68ca0ca26e26b41285698f47008ff2cc3b07d9
Adnan <adnan.silajdzic.it@gmail.com>
2023-04-21 19:40:56 +0200

    merged with branch ACTool-2 and updated design slightly

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,82 +21,65 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="flex h-screen not-italic ">
-      <div className="w-1/5 border-r border-gray-300 bg-green-100">
-        <div className="p-8">
-          <span className="font-medium">HOME</span>
-        </div>
-      </div>
-      <div className="page-content ml-4 mr-4 flex-1 p-4">
-        <div className="flex-1 p-8 py-4 text-2xl text-3xl font-bold">Home</div>
-        <div className="content-categories flex">
-          <div className="label-wrapper"></div>
-          <div className="flex-1 p-8 py-2">
-            <div className="flex items-center justify-between py-4">
-              <div className="flex">
-                <div className="relative">
-                  <input
-                    type="radio"
-                    name="nav"
-                    id="opt-1"
-                    className="sr-only"
-                  />
-                  <label
-                    htmlFor="opt-1"
-                    className="cursor-pointer rounded-l-md border border-gray-400 px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-green-100 focus:bg-[#F5FFFA]"
-                  >
-                    2023 Performance
-                  </label>
-                </div>
-                <div className="relative">
-                  <input
-                    type="radio"
-                    name="nav"
-                    id="opt-2"
-                    className="sr-only"
-                    defaultChecked
-                  />
-                  <label
-                    htmlFor="opt-2"
-                    className="cursor-pointer border border-gray-400 px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-green-100 focus:bg-[#F5FFFA]"
-                  >
-                    Development Revenue &amp; Costs
-                  </label>
-                </div>
-                <div className="relative">
-                  <input
-                    type="radio"
-                    name="nav"
-                    id="opt-3"
-                    className="sr-only"
-                  />
-                  <label
-                    htmlFor="opt-3"
-                    className="cursor-pointer rounded-r border border-gray-400 px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-green-100 focus:bg-[#F5FFFA]"
-                  >
-                    2023 Plan
-                  </label>
-                </div>
-              </div>
-              <div className="mr-5 flex items-center">
-                <label htmlFor="years" className="mr-4 font-medium">
-                  Year:
-                </label>
-                <select
-                  name="years"
-                  id="years"
-                  className="rounded-md border border-gray-400 px-4 py-2 font-medium"
+    <div className="page-content ml-4 mr-4 flex-1 p-4">
+      <div className="flex-1 p-8 py-4 text-2xl text-3xl font-bold">Home</div>
+      <div className="content-categories flex">
+        <div className="label-wrapper"></div>
+        <div className="flex-1 p-8 py-2">
+          <div className="flex items-center justify-between py-4">
+            <div className="flex">
+              <div className="relative">
+                <input type="radio" name="nav" id="opt-1" className="sr-only" />
+                <label
+                  htmlFor="opt-1"
+                  className="cursor-pointer rounded-l-md border border-[#B3DFC9] px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-[#F5FFFA] focus:bg-[#F5FFFA]"
                 >
-                  <option value="2023">2023</option>
-                  <option value="2022">2022</option>
-                  <option value="2021">2021</option>
-                  <option value="2020">2020</option>
-                </select>
+                  2023 Performance
+                </label>
+              </div>
+              <div className="relative">
+                <input
+                  type="radio"
+                  name="nav"
+                  id="opt-2"
+                  className="sr-only"
+                  defaultChecked
+                />
+                <label
+                  htmlFor="opt-2"
+                  className="cursor-pointer border-y border-[#B3DFC9] px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-[#F5FFFA] focus:bg-[#F5FFFA]"
+                >
+                  Development Revenue &amp; Costs
+                </label>
+              </div>
+              <div className="relative">
+                <input type="radio" name="nav" id="opt-3" className="sr-only" />
+                <label
+                  htmlFor="opt-3"
+                  className="cursor-pointer rounded-r border border-[#B3DFC9] px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-[#F5FFFA] focus:bg-[#F5FFFA]"
+                >
+                  2023 Plan
+                </label>
               </div>
             </div>
-            <div className="inline w-1/5 justify-center">
-              <h1 className="mt-6 text-2xl font-bold" id="content-heading"></h1>
+            <div className="mr-5 flex items-center">
+              <label htmlFor="years" className="mr-4 font-medium">
+                Year:
+              </label>
+              <select
+                name="years"
+                id="years"
+                className="rounded-md border border-gray-400 px-4 py-2 font-medium"
+              >
+                <option value="2023">2023</option>
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+              </select>
             </div>
+          </div>
+          <div className="inline w-1/5 justify-center">
+            <h1 className="mt-6 text-2xl font-bold" id="content-heading"></h1>
           </div>
         </div>
       </div>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -26,88 +26,66 @@ const Projects = () => {
   }, []);
 
   return (
-    <div className="flex h-screen not-italic">
-      <div className="w-1/5 border-r border-gray-300 bg-green-100">
-        <div className="p-8">
-          <span className="font-medium">PROJECTS</span>
+    <div className="page-content ml-4 mr-8 flex-1 p-4">
+      <div className="content-categories flex items-center">
+        <div className="flex-1 p-8 py-4 text-2xl text-3xl font-bold">
+          Projects
         </div>
+        <button className="ml-auto rounded-md bg-buttonColor px-4 py-2 text-base font-semibold text-white hover:bg-green-900">
+          Create new project
+        </button>
       </div>
-      <div className="page-content ml-4 mr-8 flex-1 p-4">
-        <div className="content-categories flex items-center">
-          <div className="flex-1 p-8 py-4 text-2xl text-3xl font-bold">
-            Projects
-          </div>
-          <button className="ml-auto rounded-md bg-buttonColor px-4 py-2 text-base font-semibold text-white hover:bg-green-900">
-            Create new project
-          </button>
-        </div>
-        <div className="content-categories flex">
-          <div className="label-wrapper"></div>
-          <div className="flex-1 p-8 py-2">
-            <div className="flex items-center justify-between py-4">
-              <div className="flex">
-                <div className="relative">
-                  <input
-                    type="radio"
-                    name="nav"
-                    id="opt-1"
-                    className="sr-only"
-                  />
-                  <label
-                    htmlFor="opt-1"
-                    className="cursor-pointer rounded-l-md border border-gray-400 px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-green-100 focus:bg-[#F5FFFA]"
-                  >
-                    All projects
-                  </label>
-                </div>
-                <div className="relative">
-                  <input
-                    type="radio"
-                    name="nav"
-                    id="opt-2"
-                    className="sr-only"
-                    defaultChecked
-                  />
-                  <label
-                    htmlFor="opt-2"
-                    className="cursor-pointer border border-gray-400 px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-green-100 focus:bg-green-100"
-                  >
-                    Active
-                  </label>
-                </div>
-                <div className="relative">
-                  <input
-                    type="radio"
-                    name="nav"
-                    id="opt-3"
-                    className="sr-only"
-                  />
-                  <label
-                    htmlFor="opt-3"
-                    className="cursor-pointer  border border-gray-400 px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-green-100 focus:bg-green-100"
-                  >
-                    Inactive
-                  </label>
-                </div>
-                <div className="relative">
-                  <input
-                    type="radio"
-                    name="nav"
-                    id="opt-4"
-                    className="sr-only"
-                  />
-                  <label
-                    htmlFor="opt-4"
-                    className="cursor-pointer rounded-r-md border border-gray-400 px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-green-100 focus:bg-green-100"
-                  >
-                    Completed
-                  </label>
-                </div>
+      <div className="content-categories flex">
+        <div className="label-wrapper"></div>
+        <div className="flex-1 p-8 py-2">
+          <div className="flex items-center justify-between py-4">
+            <div className="flex">
+              <div className="relative">
+                <input type="radio" name="nav" id="opt-1" className="sr-only" />
+                <label
+                  htmlFor="opt-1"
+                  className="cursor-pointer rounded-l-md border border-[#B3DFC9] px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-[#F5FFFA] focus:bg-[#F5FFFA]"
+                >
+                  All projects
+                </label>
+              </div>
+              <div className="relative">
+                <input
+                  type="radio"
+                  name="nav"
+                  id="opt-2"
+                  className="sr-only"
+                  defaultChecked
+                />
+                <label
+                  htmlFor="opt-2"
+                  className="cursor-pointer border-y border-r border-[#B3DFC9] px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-[#F5FFFA] focus:bg-green-100"
+                >
+                  Active
+                </label>
+              </div>
+              <div className="relative">
+                <input type="radio" name="nav" id="opt-3" className="sr-only" />
+                <label
+                  htmlFor="opt-3"
+                  className="cursor-pointer  border-y border-[#B3DFC9] px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-[#F5FFFA] focus:bg-green-100"
+                >
+                  Inactive
+                </label>
+              </div>
+              <div className="relative">
+                <input type="radio" name="nav" id="opt-4" className="sr-only" />
+                <label
+                  htmlFor="opt-4"
+                  className="cursor-pointer rounded-r-md border border-[#B3DFC9] px-4 py-2 text-sm font-semibold text-[#43A57C] transition-colors duration-200 ease-in-out hover:bg-[#F5FFFA] focus:bg-green-100"
+                >
+                  Completed
+                </label>
               </div>
             </div>
-            <div className="inline w-1/5 justify-center">
-              <h1 className="mt-6 text-2xl font-bold" id="content-heading"></h1>
-            </div>
+          </div>
+          <div className="inline w-1/5 justify-center">
+            <h1 className="mt-6 text-2xl font-bold" id="content-heading"></h1>
           </div>
         </div>
       </div>
