ac6ec126c1917d631865eec3030569a6ab1ada3e
tdogic2 <tdogic2@etf.unsa.ba>
2023-05-11 13:55:47 +0200

    Fixed background color of pagination buttons and their padding.

--- src/components/projects/pagination/PageNumberButton.tsx
+++ src/components/projects/pagination/PageNumberButton.tsx
@@ -7,8 +7,8 @@ type Props = {
 const PageNumberButton = ({ pageNumber, isActive, onClick }: Props) => {
 	return (
 		<button
-			className={`h-8 rounded border border-silvery-sky px-2 font-["Open_Sans"] text-sm  ${
-				isActive ? 'bg-silvery-sky font-semibold text-shadow-slate' : 'text-opacity-45 bg-cloudy-pearl text-black'
+			className={`h-8 rounded border border-silvery-sky px-3 font-["Open_Sans"] text-sm  ${
+				isActive ? 'bg-cloudy-pearl font-semibold text-shadow-slate' : 'text-opacity-45 bg-white text-black'
 			}`}
 			onClick={onClick}
 		>
