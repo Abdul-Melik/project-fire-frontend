78acf0507f0e953ab41cdd3377b88f1bc6c96974
har1zarD <haris.velic@ses.edu.ba>
2023-05-30 12:15:44 +0200

    Add Employees design

--- src/shared/components/table-elements/TableRow.tsx
+++ src/shared/components/table-elements/TableRow.tsx
@@ -1,18 +1,29 @@
 type Props = {
-	className?: string;
-	onClick?: (event: React.MouseEvent<HTMLTableRowElement>) => void;
-	children: React.ReactNode;
+  className?: string;
+  onClick?: (event: React.MouseEvent<HTMLTableRowElement>) => void;
+  children: React.ReactNode;
 };
 
 const TableRow = ({ className, onClick, children }: Props) => {
-	return (
-		<tr
-			className={`h-[60px] border-t border-ashen-grey text-left font-gilroy-regular text-sm font-normal leading-[22px] text-slate-mist ${className}`}
-			onClick={onClick}
-		>
-			{children}
-		</tr>
-	);
+  return (
+    <tr
+      className={`h-[60px] border-t border-ashen-grey text-left font-gilroy-regular text-sm font-normal leading-[22px] text-slate-mist ${className}`}
+      onClick={onClick}
+      style={{
+        transition: "transform 0.3s",
+        willChange: "transform",
+        cursor: "pointer",
+      }}
+      onMouseEnter={(e) => {
+        e.currentTarget.style.transform = "translateX(-4px)";
+      }}
+      onMouseLeave={(e) => {
+        e.currentTarget.style.transform = "translateX(0)";
+      }}
+    >
+      {children}
+    </tr>
+  );
 };
 
 export default TableRow;
