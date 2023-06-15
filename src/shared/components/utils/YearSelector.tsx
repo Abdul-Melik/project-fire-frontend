d8e6b7cd1ae9f506142e0c902d4ef7f26a3d3722
Adnan <adnan.silajdzic.it@gmail.com>
2023-06-15 11:19:36 +0200

    refactored year select to match design

--- src/shared/components/utils/YearSelector.tsx
+++ src/shared/components/utils/YearSelector.tsx
@@ -1,35 +1,74 @@
-import { useState } from "react";
+import { useState } from 'react';
+import Select from 'react-select';
 
 type Props = {
-  handleYearSelect: (year: string) => void;
-  label: string;
-  options: string[];
-  defaultValue: string;
+	handleYearSelect: (year: string) => void;
+	label: string;
+	options: string[];
+	defaultValue: string;
 };
 
 const YearSelector = ({ handleYearSelect, label, options, defaultValue }: Props) => {
-  const [selectedYear, setSelectedYear] = useState<string>("");
+	const [selectedYear, setSelectedYear] = useState<string>('');
 
-  return (
-    <div className='flex items-center'>
-      <label htmlFor='years' className='mr-4 font-gilroy-bold text-[22px] font-bold leading-[30px] text-deep-forest'>
-        {label}:
-      </label>
-      <select
-        className='flex justify-between gap-[10px] rounded-md border border-ashen-grey py-2 pl-[12px] pr-[10px] font-gilroy-bold text-base font-bold text-hunter-green'
-        id='years'
-        value={selectedYear}
-        onChange={(event) => {
-          handleYearSelect(event.target.value);
-          setSelectedYear(event.target.value);
-        }}
-      >
-        {options.map((option) => (
-          <option value={option}>{option}</option>
-        ))}
-      </select>
-    </div>
-  );
+	const customStyles = {
+		control: (provided: any) => ({
+			...provided,
+			width: '120px',
+			height: '40px',
+			fontFamily: 'Gilroy-Bold',
+			backgroundColor: 'white',
+			border: '1px solid #DFE3E1',
+			borderRadius: '6px',
+			paddingLeft: '8px',
+			paddingRight: '10px',
+			fontSize: '16px',
+			fontWeight: 'bold',
+			color: 'black',
+		}),
+		option: (provided: any, state: any) => {
+			const isLastOption = options.indexOf(state.data.value) === options.length - 1;
+			const borderBottom = isLastOption ? '0' : '1px solid #DFE3E1';
+			return {
+				...provided,
+				backgroundColor: state.isFocused ? '#EFF8F7' : 'white',
+				color: state.isFocused ? 'black' : 'inherit',
+				fontSize: '1rem',
+				fontFamily: 'Gilroy-Medium',
+				fontWeight: '500',
+				borderBottom,
+			};
+		},
+		menu: (provided: any) => ({
+			...provided,
+			zIndex: 9999,
+			width: '160px',
+			marginLeft: '-40px',
+		}),
+	};
+
+	return (
+		<div className='flex items-center'>
+			<label htmlFor='years' className='mr-4 font-gilroy-bold text-[22px] font-bold leading-[30px] text-deep-forest'>
+				{label}:
+			</label>
+			<Select
+				id='years'
+				defaultInputValue='2023'
+				isSearchable={false}
+				components={{
+					IndicatorSeparator: () => null,
+				}}
+				options={options.map(option => ({ value: option, label: option }))}
+				value={{ value: selectedYear, label: selectedYear }}
+				onChange={(selectedOption: any) => {
+					handleYearSelect(selectedOption.value);
+					setSelectedYear(selectedOption.value);
+				}}
+				styles={customStyles}
+			/>
+		</div>
+	);
 };
 
 export default YearSelector;
