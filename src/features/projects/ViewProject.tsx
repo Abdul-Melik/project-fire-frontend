a7d04e24375cdf39d49d5091a875e06855646e91
tdogic2 <tdogic2@etf.unsa.ba>
2023-06-24 10:26:06 +0200

    Project info refactoring.

--- src/features/projects/ViewProject.tsx
+++ src/features/projects/ViewProject.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { Project } from 'src/types';
-import { getProjectDate, getProjectValueBAM, getProjectColorAndStatus } from 'src/helpers';
 import { useAppSelector } from 'store/hooks';
 import { selectUserRole } from 'store/slices/authSlice';
 import { useDeleteProjectMutation } from 'store/slices/projectsApiSlice';
@@ -12,6 +11,7 @@ import SideDrawer from 'components/navigation/SideDrawer';
 import Header from 'components/layout/Header';
 import Main from 'components/layout/Main';
 import Footer from 'components/layout/Footer';
+import ProjectInfo from 'features/projects/ProjectInfo';
 
 type Props = {
 	project: Project;
@@ -64,57 +64,7 @@ const ViewProject = ({ project, closeViewProjectSideDrawer, openEditProjectSideD
 							</h2>
 						</Header>
 						<Main className='flex flex-col gap-5'>
-							<div className='flex flex-col gap-4 rounded-lg bg-white p-6'>
-								<div className='flex flex-col border-b border-ashen-grey pb-4'>
-									<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Name</span>
-									<span className='font-gilroy-regular text-base font-normal text-slate-mist'>{project.name}</span>
-								</div>
-								<div className='flex flex-col border-b border-ashen-grey pb-4'>
-									<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Description</span>
-									<span className='font-gilroy-regular text-base font-normal text-slate-mist'>
-										{project.description}
-									</span>
-								</div>
-								<div className='flex flex-col border-b border-ashen-grey pb-4'>
-									<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Duration</span>
-									<span className='font-gilroy-regular text-base font-normal text-slate-mist'>
-										{getProjectDate(project.startDate, project.endDate)}
-									</span>
-								</div>
-								<div className='flex flex-col border-b border-ashen-grey pb-4'>
-									<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Team members</span>
-									<span className='font-gilroy-regular text-base font-normal text-slate-mist'>
-										{project.employees.map(({ employee }) => `${employee.firstName} ${employee.lastName}`).join(', ')}
-									</span>
-								</div>
-								<div className='flex flex-col border-b border-ashen-grey pb-4'>
-									<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Hourly Rate (USD)</span>
-									<span className='font-gilroy-regular text-base font-normal text-slate-mist'>
-										{project.hourlyRate}
-									</span>
-								</div>
-								<div className='flex flex-col border-b border-ashen-grey pb-4'>
-									<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>
-										Project Value (BAM)
-									</span>
-									<span className='font-gilroy-regular text-base font-normal text-slate-mist'>
-										{getProjectValueBAM(project.projectValueBAM)}
-									</span>
-								</div>
-								<div className='flex flex-col'>
-									<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Status</span>
-									<div className='flex items-center gap-2'>
-										<div
-											className={`h-[6px] w-[6px] rounded-full ${
-												getProjectColorAndStatus(project.projectStatus)?.color
-											}`}
-										/>
-										<div className='font-gilroy-regular font-normal text-slate-mist'>
-											{getProjectColorAndStatus(project.projectStatus)?.status}
-										</div>
-									</div>
-								</div>
-							</div>
+							<ProjectInfo project={project} />
 						</Main>
 						{userRole === 'Admin' && (
 							<Footer
