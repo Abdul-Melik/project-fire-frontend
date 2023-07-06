cf62c0dd181a287da09b2d838638b8fb19024efe
har1zarD <haris.velic@ses.edu.ba>
2023-07-06 10:17:58 +0200

    Sales, Type, Actual EndDate on projects.

---
+++ src/components/formElements/DateInput.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import CustomDateInput from "components/formElements/CustomDateInput";
+
+type Props = {
+  endDate: Date | null;
+  actualEndDate: Date | null;
+  handleActualEndDateInput: (date: Date | null) => void;
+};
+
+const DateInput = ({ endDate, actualEndDate, handleActualEndDateInput }: Props) => {
+  return (
+    <div className='flex flex-col gap-1'>
+      <span className='font-gilroy-medium text-base font-medium leading-[22px] text-midnight-grey'>
+        Actual End Date
+      </span>
+      <DatePicker
+        customInput={<CustomDateInput width='w-full' />}
+        placeholderText={endDate?.toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "short",
+        })}
+        selected={actualEndDate || endDate}
+        onChange={(date) => handleActualEndDateInput(date)}
+      />
+    </div>
+  );
+};
+
+export default DateInput;
--- src/features/projects/ProjectInfo.tsx
+++ src/features/projects/ProjectInfo.tsx
@@ -1,54 +1,70 @@
-import { Project } from 'src/types';
-import { getProjectDate, getProjectValueBAM, getProjectColorAndStatus } from 'src/helpers';
+import { Project } from "src/types";
+import { getProjectDate, getProjectValueBAM, getProjectActualEndDate, getProjectColorAndStatus } from "src/helpers";
 
 type Props = {
-	project: Project;
+  project: Project;
 };
 
 const ProjectInfo = ({ project }: Props) => {
-	return (
-		<div className='flex flex-col gap-4 rounded-lg bg-white p-6'>
-			<div className='flex flex-col border-b border-ashen-grey pb-4'>
-				<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Name</span>
-				<span className='font-gilroy-regular text-base font-normal text-slate-mist'>{project.name}</span>
-			</div>
-			<div className='flex flex-col border-b border-ashen-grey pb-4'>
-				<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Description</span>
-				<span className='font-gilroy-regular text-base font-normal text-slate-mist'>{project.description}</span>
-			</div>
-			<div className='flex flex-col border-b border-ashen-grey pb-4'>
-				<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Duration</span>
-				<span className='font-gilroy-regular text-base font-normal text-slate-mist'>
-					{getProjectDate(project.startDate, project.endDate)}
-				</span>
-			</div>
-			<div className='flex flex-col border-b border-ashen-grey pb-4'>
-				<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Team members</span>
-				<span className='font-gilroy-regular text-base font-normal text-slate-mist'>
-					{project.employees.map(({ employee }) => `${employee.firstName} ${employee.lastName}`).join(', ')}
-				</span>
-			</div>
-			<div className='flex flex-col border-b border-ashen-grey pb-4'>
-				<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Hourly Rate (USD)</span>
-				<span className='font-gilroy-regular text-base font-normal text-slate-mist'>{project.hourlyRate}</span>
-			</div>
-			<div className='flex flex-col border-b border-ashen-grey pb-4'>
-				<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Project Value (BAM)</span>
-				<span className='font-gilroy-regular text-base font-normal text-slate-mist'>
-					{getProjectValueBAM(project.projectValueBAM)}
-				</span>
-			</div>
-			<div className='flex flex-col'>
-				<span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Status</span>
-				<div className='flex items-center gap-2'>
-					<div className={`h-[6px] w-[6px] rounded-full ${getProjectColorAndStatus(project.projectStatus)?.color}`} />
-					<div className='font-gilroy-regular font-normal text-slate-mist'>
-						{getProjectColorAndStatus(project.projectStatus)?.status}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const endDate = project.actualEndDate ? project.actualEndDate : project.endDate;
+
+  return (
+    <div className='flex flex-col gap-4 rounded-lg bg-white p-6'>
+      <div className='flex flex-col border-b border-ashen-grey pb-4'>
+        <span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Name</span>
+        <span className='font-gilroy-regular text-base font-normal text-slate-mist'>{project.name}</span>
+      </div>
+      <div className='flex flex-col border-b border-ashen-grey pb-4'>
+        <span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Description</span>
+        <span className='font-gilroy-regular text-base font-normal text-slate-mist'>{project.description}</span>
+      </div>
+      <div className='flex flex-col border-b border-ashen-grey pb-4'>
+        <span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Duration</span>
+        <span className='font-gilroy-regular text-base font-normal text-slate-mist'>
+          {getProjectDate(project.startDate, project.endDate)}
+        </span>
+      </div>
+      <div className='flex flex-col border-b border-ashen-grey pb-4'>
+        <span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Actual End Date</span>
+        <span className='font-gilroy-regular text-base font-normal text-slate-mist'>
+          {getProjectActualEndDate(endDate)}
+        </span>
+      </div>
+      <div className='flex flex-col border-b border-ashen-grey pb-4'>
+        <span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Team members</span>
+        <span className='font-gilroy-regular text-base font-normal text-slate-mist'>
+          {project.employees.map(({ employee }) => `${employee.firstName} ${employee.lastName}`).join(", ")}
+        </span>
+      </div>
+      <div className='flex flex-col border-b border-ashen-grey pb-4'>
+        <span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Hourly Rate (USD)</span>
+        <span className='font-gilroy-regular text-base font-normal text-slate-mist'>{project.hourlyRate}</span>
+      </div>
+      <div className='flex flex-col border-b border-ashen-grey pb-4'>
+        <span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Project Value (BAM)</span>
+        <span className='font-gilroy-regular text-base font-normal text-slate-mist'>
+          {getProjectValueBAM(project.projectValueBAM)}
+        </span>
+      </div>
+      <div className='flex flex-col border-b border-ashen-grey pb-4'>
+        <span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Status</span>
+        <div className='flex items-center gap-2'>
+          <div className={`h-[6px] w-[6px] rounded-full ${getProjectColorAndStatus(project.projectStatus)?.color}`} />
+          <div className='font-gilroy-regular font-normal text-slate-mist'>
+            {getProjectColorAndStatus(project.projectStatus)?.status}
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-col border-b border-ashen-grey pb-4'>
+        <span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Project Type</span>
+        <span className='font-gilroy-regular text-base font-normal text-slate-mist'>{project.projectType}</span>
+      </div>
+      <div className='flex flex-col'>
+        <span className='font-gilroy-medium text-base font-medium text-midnight-grey'>Sales Channel</span>
+        <span className='font-gilroy-regular text-base font-normal text-slate-mist'>{project.salesChannel}</span>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectInfo;
